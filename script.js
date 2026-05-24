19ad7e78a309c26dedc8f91d6d1481286113f6fd
FabioJRR-Escola <rocha.junior.fabio@escola.pr.gov.br>
2026-05-24 16:35:18 -0300

    Criar script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+if(!localStorage.getItem('produtores')) {
+    localStorage.setItem('produtores', JSON.stringify([]));
+}
+
+const produtores = JSON.parse(localStorage.getItem('produtores'));
+
+const listaProdutores = document.getElementById('lista-produtores');
+const listaProdutos = document.getElementById('lista-produtos');
+
+produtores.forEach(produtor => {
+
+    const cardProdutor = document.createElement('div');
+    cardProdutor.classList.add('card');
+
+    cardProdutor.innerHTML = `
+        <img src="${produtor.imagem}">
+
+        <h3>${produtor.nome}</h3>
+
+        <p>${produtor.historia}</p>
+
+        <p><strong>Produto:</strong> ${produtor.produto}</p>
+    `;
+
+    listaProdutores.appendChild(cardProdutor);
+
+    const cardProduto = document.createElement('div');
+    cardProduto.classList.add('card');
+
+    cardProduto.innerHTML = `
+        <img src="${produtor.imagem}">
+
+        <h3>${produtor.produto}</h3>
+
+        <p>Produzido por ${produtor.nome}</p>
+    `;
+
+    listaProdutos.appendChild(cardProduto);
+});
